dd257ba32b9eab41e7092a9edb9e34967c239946
Amrit Prakash <110738787+iamamritjha@users.noreply.github.com>
2025-11-09 23:37:51 +0530

    Solved

---
+++ Matrix.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public: 
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> row(m, 0), col(n, 0);
+
+        // Step 1: Mark the rows and columns that need to be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Step 2: Set matrix cells to 0 based on markers
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<vector<int>> matrix = {
+        {1, 1, 1},
+        {1, 0, 1},
+        {1, 1, 1}
+    };
+
+    sol.setZeroes(matrix);
+
+    // Print the result
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
